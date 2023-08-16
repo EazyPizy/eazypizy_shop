4d01402e1170cf15c89588388e00b0896c6742cc
harsh-scrobits <harshkumar.scrobits@gmail.com>
2023-08-16 21:37:56 +0530

    added example project

--- lib/eazypizy_shop.dart
+++ lib/eazypizy_shop.dart
@@ -1,7 +1,21 @@
 library eazypizy_shop;
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class ShopHomeWidget extends StatelessWidget {
+  const ShopHomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // body: const Center(child: Text('some'.tr),),
+        );
+  }
 }
